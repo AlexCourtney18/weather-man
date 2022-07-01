5b7c5d15e690b198c7913d7787769fccd4402624
Alex <lex.courtney18@gmail.com>
2022-06-30 22:54:02 -0500

    fix grid with html classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city");
+var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Milwaukee&appid=67820c596372ebc8bcdbff3f5b73527a"
+
+
+
+
+
+fetch(apiUrl)
+.then(res => res.json())
+.then(data => console.log(data))
+
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var city = cityInputEl.value.trim();
+    console.log(city);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+
+
 // var Weather = function (repo) {
 // var apiUrl = ;
 
@@ -6,14 +46,9 @@
 //         console.log
 //     }
 // }
-var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Milwaukee&appid=67820c596372ebc8bcdbff3f5b73527a"
 
 
 
 // (fetch("https://api.openweathermap.org/data/2.5/weather?q=Milwaukee&appid=67820c596372ebc8bcdbff3f5b73527a"))
 // .then(res => res.json())
 // .then(data => console.log(data))
-
-fetch(apiUrl)
-.then(res => res.json())
-.then(data => console.log(data))
